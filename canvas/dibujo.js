130953b7afa8ce59cadb69548b9063cc2c97a8da
Lolaso <lolasoquiroz@gmail.com>
2021-01-23 12:18:12 -0400

    Dibujo Bonito!

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,20 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
 
-dibujarLinea("#AAF", 0, 0, 10, 300);
+for(l = 0; l < lineas; l++)
+{
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea(colorcito, 0, yi, xf , 300);
+  console.log("Linea" + l);
+}
 
+dibujarLinea(colorcito, 1, 1, 1, 300);
+dibujarLinea(colorcito, 1, 299, 299, 299 );
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
