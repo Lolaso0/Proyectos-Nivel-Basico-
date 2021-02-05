ad4cf88db9bb0ffae7df3b03fee095df68881b0a
Lolaso <lolasoquiroz@gmail.com>
2021-02-05 15:29:12 -0400

    Iniciando ATM

---
+++ atm/cajero.js
@@ -0,0 +1,24 @@
+class Billete
+{
+  constructor(v, c)
+  {
+    this.valor = v;
+    this.cantidad = c;
+  }
+  mostrar()
+  {
+
+  }
+}
+
+
+//Definir Billetes
+var dolar = [];
+dolar[0] = new Billete(1, 10);
+dolar[1] = new Billete(5, 10);
+console.log(dolar);
+
+for(d of dolar)
+{
+  document.write("pepe cpme mofefqfaes");
+}
